b19777d1317e74c8fdb50f4cabce5c8a72168023
ohirota <ohirota2220@gmail.com>
2026-07-23 10:56:07 +0900

    プロジェクト ファイルを追加します。

---
+++ WpfApp1/Models/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int MarketPrice { get; set; }
+
+        public int Difference => Math.Abs(Price - MarketPrice);
+        public string Judgement
+        {
+            get
+            {
+                if (Difference < 3000) return "買い";
+                else if (Difference < 5000) return "見送り";
+                else return "きつい";
+            }
+        }
+    }
+}
---
+++ WpfApp1/ViewModels/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using WpfApp1.Models;
+
+
+namespace WpfApp1.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        private List<Item> _allLines = new List<Item>()
+        {
+            new Item{ Name = "アクセA", Price =1000, MarketPrice = 900},
+            new Item{ Name = "アクセB", Price =33500, MarketPrice = 37500},
+            new Item{ Name = "アクセC", Price =10000, MarketPrice = 90000}
+        };
+
+        public ObservableCollection<Item> FilteredLines { get; set; }
+
+        public MainViewModel()
+        {
+            FilteredLines = new ObservableCollection<Item>(_allLines);
+
+
+            
+        }
+    }
+}
